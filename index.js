ee7a69216bc3ad9256de241693b57df2849cf3e9
Nashrullah Ali Fauzi <114306194+nashrullahalifauzi@users.noreply.github.com>
2024-10-24 09:16:57 +0700

    add all new 3

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var light_dark = document.getElementById('light-dark');
+var lightdark = document.getElementById('light-dark');
 var light = document.getElementById('light');
 var dark = document.getElementById('dark');
 var url = q2o(window.location.search);
 if (window.location.search) {
     var skin = url.skin;
     if (skin == "light dark") {
-      light_dark.checked = true;
+      lightdark.checked = true;
     } else if (skin == "light") {
       light.checked = true;
     } else if (skin == "dark") {

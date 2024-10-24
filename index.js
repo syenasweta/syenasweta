f68d713a8050866acefca479f432ae1a510b6082
Nashrullah Ali Fauzi <114306194+nashrullahalifauzi@users.noreply.github.com>
2024-10-24 08:52:12 +0700

    add all new 3

---
+++ index.js
@@ -0,0 +1,16 @@
+var light_dark = document.getElementById('light-dark');
+var light = document.getElementById('light');
+var dark = document.getElementById('dark');
+var url = q2o(window.location.search);
+if (window.location.search) {
+    var skin = url.skin;
+    if (skin == "light dark") {
+      light_dark.checked = true;
+    } else if (skin == "light") {
+      light.checked = true;
+    } else if (skin == "dark") {
+      dark.checked = true;
+    } else {
+      light_dark.checked = true;
+    }
+}
